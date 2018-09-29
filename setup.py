54f38e3e00d2a16f091cf97e9d37d447c068ed12
Abner <abnersousanascimento@gmail.com>
2018-09-29 20:57:21 -0300

    fix badges

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='bayesian_sgm',
     version='1.0.0',
     description='Um segmentador de cores bayesiano para a OpenCV.',
     author='Abner',
     author_email='abnersousanascimento@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/abnersn/bayesian_sgm',
-    license='GNU',
-    python_requires='>=3.5.5',
-    packages=['bayesian_sgm'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.0",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: Portuguese (Brazilian)",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
-        'numpy>=1.14.3',
-        'cv2>=3.1.0'
+        "opencv-python >= 3.1.0",
+        "numpy >= 1.14.3"
     ]
 )
